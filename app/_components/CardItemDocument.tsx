9f1b5af8e2574d7641106c41bf954fec38059955
Vinicyos Ferreira <vinifr987@gmail.com>
2026-03-06 13:06:31 -0300

    feat: add Button to sign the document in case of pending issues

--- app/_components/CardItemDocument.tsx
+++ app/_components/CardItemDocument.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Lock, Clock } from "lucide-react";
 import DialogViewDocument from "./DialogViewDocument";
+import { Button } from "@/components/ui/button";
 
 const CardItemDocument = ({
   title,
@@ -14,17 +16,23 @@ const CardItemDocument = ({
 }) => {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle className="truncate">{title}</CardTitle>
+        {status === "PENDENTE" && (
+          <Button variant="outline" size="sm">
+            <Lock className="w-4 h-4 mr-2" />
+            Assinar
+          </Button>
+        )}
       </CardHeader>
-      <CardContent className="space-y-2">
-        <div>
-          <p className="line-clamp-5">{description}</p>
+      <CardContent className="space-y-2 flex flex-col h-full">
+        <div className="flex-1">
+          <p className="line-clamp-2">{description}</p>
         </div>
 
         <DialogViewDocument title={title} description={description} />
 
-        <div className="flex justify-end">
+        <div className="flex justify-end mt-1">
           {status === "PENDENTE" ? (
             <Badge variant="destructive">
               <span className="font-bold">Pendente</span>
